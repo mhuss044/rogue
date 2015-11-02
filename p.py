e7d27d7bde9533bb9c8b4885b8ba9a8e1ec3444e
mhuss044D430 <mhuss044@gmail.com>
2015-11-01 19:39:44 -0500

    Final features include title screen takes your name,

---
+++ p.py
@@ -0,0 +1,7 @@
+e = []
+for i in range(1, 50):
+	e.append(i)
+for i in range(1, 50):
+	print e[i]
+for i in range(1, 50, 3):
+	print e[i]
--- rogue.py
+++ rogue.py
@@ -5,6 +5,9 @@
 		Inventory
 		Character equip from ground
 		Trade with vendors
+
+		on die anim; circle of chars animated draw
+			then chars with deg same rotate colour from 1-7
 '''
 '''
 
@@ -59,21 +62,6 @@
 				     					# pass does nothing, only placed if syntax requires a statement
 			 		 
 
-		# Key input needs to be in another thread, bcs waiting for prog to get out of nap
-		if stdscr.getch() == ord('w'):
-			fireEngines = True
-		if stdscr.getch() == ord('s'):
-			fireEngines = False
-		if stdscr.getch() == ord('r'):
-			if speed > 50:
-				speed -= 100
-		if stdscr.getch() == ord('f'):
-			if speed < 500:
-				speed += 100
-
-	oriWin = gameWindow( winDivide, int(curses.LINES/3), int(curses.LINES/3), 40)
-	oriWin.makeWin( curses.newwin( oriWin.height, oriWin.width, oriWin.yTop, oriWin.xTop))
-
 	ammoStatusWin = gameWindow( winDivide, int(curses.LINES/3)*2, int(curses.LINES/3), 40)
 	ammoStatusWin.makeWin( curses.newwin( ammoStatusWin.height, ammoStatusWin.width, ammoStatusWin.yTop, ammoStatusWin.xTop))
 
@@ -88,15 +76,46 @@ from math import cos
 from math import radians
 import curses	# curses lib
 from curses import wrapper
+from curses.textpad import Textbox
 
 random.seed()	# seed with current system time
 mainWindef = { 'xTop' : 0, 'yTop' : 0, 'height' : 1, 'width' : 1, 'mainWin' : 0 }  
 
+class gameInfo(object):
+	def __init__(self, state, x):
+		self.enemies = []
+		self.curLevel = 0
+		self.gameState = state		# true; playing game, false; game over; title screen 
+		self.maxX = x
+	def setmaxLvl(self, lvl):
+		self.maxLevels = lvl
+	def advLvl():
+		if(self.curLevel < self.maxLevels):
+			self.curLevel += 1;
+		else:
+			self.curLevel = 0
+	def startNewGame(self):
+		self.curLevel = 0
+		self.gameState = True
+
+#		Setup enemies
+		for i in range(0, 20):		# set x = y = 2
+			self.enemies.append(2)
+		for i in range(0, 20, 2):	# set x to rand in
+			self.enemies[i-1] = random.randint(1, self.maxX)
+
+	def gameOver(self):
+		self.curLevel = 0
+		self.gameState = False
+
 class player(object):
-	def __init__(self, x, y, hp):
+	def __init__(self, x, y, hp, pName):
 		self.xPos = x
 		self.yPos = y
 		self.hitPoints = hp
+		self.playerName = pName 
+	def setName(self, pName):
+		self.playerName = pName
 	def takeDamage(self, dmg):
 		if(self.hitPoints > dmg):
 			self.hitPoints -= dmg
@@ -119,18 +138,32 @@ class gameWindow(object):
 #		return true
 	
 def main(stdscr):
+	startAnim = True
 	stdscr = curses.initscr()	# init curses, return a window obj
 	curses.start_color()		# init default colour set, wrapper should have done this
 	stdscr.clearok(1)		# makes next call to refresh clear the window as well
 	hudXPos = curses.COLS - 40	# x pos of HUDs to draw
 
+	newGame = gameInfo(False, curses.COLS - 40)	# false; goto title screen, num levels to pass
+	newPlayer = player( int(curses.COLS/2), curses.LINES - 2, 100, "New Player")
+
 	# setup main window where stuff coming at player is drawn, fire into mid of screen
 	mainWin = gameWindow( 0, 0, curses.LINES, hudXPos)
 	mainWin.makeWin( curses.newwin( mainWin.height, mainWin.width, mainWin.yTop, mainWin.xTop))
 
+	titleScreen = gameWindow( 0, 0, curses.LINES, curses.COLS)
+	titleScreen.makeWin( curses.newwin( titleScreen.height, titleScreen.width, titleScreen.yTop, titleScreen.xTop))
+
+	editwin = curses.newwin( 3, 12, int(titleScreen.height/2), int(titleScreen.width/2))	# edit box with height 3, w 12, mid screen
+	box = Textbox(editwin)
+
 	# set HUD windows
 	statsWin = gameWindow( hudXPos, 0, int(curses.LINES/3), 40)
 	statsWin.makeWin( curses.newwin( statsWin.height, statsWin.width, statsWin.yTop, statsWin.xTop))
+
+	mapWin = gameWindow( hudXPos, int(curses.LINES/3), int(curses.LINES/3), 40)
+	mapWin.makeWin( curses.newwin( mapWin.height, mapWin.width, mapWin.yTop, mapWin.xTop))
+
 
 	# Set some properties
 	curses.noecho()			# do not output inputted char to screen
@@ -138,34 +171,133 @@ def main(stdscr):
 	curses.curs_set(False)		# no blinking cursor
 	stdscr.keypad(True)		# allows recog of LEY_LEFT, PAGE_UP
 	stdscr.nodelay(True)		# make getch(), and getKey non block
-	strr = "fffff"
+
+# 	Define colours; for some reason, they arent defined already, access to 0 - 7
+	curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)	# colorID to change, forground, background
+	curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)	# colorID to change, forground, background
+	curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)	# colorID to change, forground, background
+	curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)	# colorID to change, forground, background
+	curses.init_pair(5, curses.COLOR_MAGENTA, curses.COLOR_BLACK)	# colorID to change, forground, background
+	curses.init_pair(6, curses.COLOR_CYAN, curses.COLOR_BLACK)	# colorID to change, forground, background
+	curses.init_pair(7, curses.COLOR_WHITE, curses.COLOR_BLACK)	# colorID to change, forground, background
+
+#	Set bkg to black
+	mainWin.winObj.bkgd(' ', curses.color_pair(0))
+
+	var = 1
+	x = 1
+	y = 1
+	rows = 20
+	toadd = 0.1
 
 	while( (stdscr.getch() != ord('q')) and (stdscr.getch != curses.KEY_EXIT) ):
-#		Clear screen so to draw next frame; future performance; draw only whats req
-		mainWin.winObj.erase()
+		if(newGame.gameState):
+			if startAnim:
+				titleScreen.winObj.erase()
+				if rows < 120:
+					rows += 2
+				else:
+					rows = 20
+				for r in range(1, rows):
+					for ang in range(0, 360):
+						x = int(titleScreen.width/2 + r*cos(radians(ang)))
+						y = int(titleScreen.height/2 + r*sin(radians(ang)))
+						if titleScreen.winObj.enclose(y, x):	# check that provided coords are in window, so no error`
+							titleScreen.winObj.addch(y, x, ord('@'), curses.color_pair(var))
+					if var < 7:
+						var += 1
+					else:
+						var = 1
 
-		mainWin.winObj.addch(int(curses.LINES/2), int(curses.COLS/2), ord('*'), curses.A_STANDOUT)
-		mainWin.winObj.addch(10, 10, ord('t'))
-#		statsWin.winObj.addnstr(y, x, str, n[, attr])
-		statsWin.winObj.addnstr(1, 1, "playerName", 10)
+				titleScreen.winObj.addnstr(int(titleScreen.height/2), int(titleScreen.width/2), "Get ready for Fun game!!!", 40, curses.color_pair(4))
+				titleScreen.winObj.addnstr(int(titleScreen.height/2)+1, int(titleScreen.width/2), "Press t to start!", 40, curses.color_pair(4))
+
+				if stdscr.getch() == ord('t'):
+					startAnim = not startAnim
+
+
+				titleScreen.winObj.border()	# use default lines, and corners	
+				titleScreen.winObj.refresh()
+				stdscr.refresh()
+
+
+			else:
+	#			Clear screen so to draw next frame; future performance; draw only whats req
+				mainWin.winObj.erase()
+
+#				Draw player:
+				if mainWin.winObj.enclose(newPlayer.yPos, newPlayer.xPos):	# check that provided coords are in window, so no error`
+					mainWin.winObj.addch(newPlayer.yPos, newPlayer.xPos, ord('@'), curses.A_STANDOUT)
+#				Draw Enemies:
+				for i in range(1, 20, 2): 
+					if mainWin.winObj.enclose( newGame.enemies[i], newGame.enemies[i-1]):
+						mainWin.winObj.addch(newGame.enemies[i], newGame.enemies[i-1], ord('e'))
+				for i in range(1, 20, 2): 
+					if(newGame.enemies[i] + 2 < curses.LINES - 1):
+						toadd += 0.0001
+						newGame.enemies[i] += int(toadd)
+					else:
+						startAnim = not startAnim
+#				statsWin.winObj.addnstr(y, x, str, n[, attr])
+				statsWin.winObj.addnstr(1, 1, newPlayer.playerName, 10, curses.color_pair(4))
+				statsWin.winObj.addnstr(2, 1, "Health:"+str(newPlayer.hitPoints), 20, curses.color_pair(4))
+				statsWin.winObj.addnstr(3, 1, "Damage:", 10, curses.color_pair(4))
+
+						
+				mapWin.winObj.addnstr(1, 1, "Map", 10, curses.color_pair(5))
+				mapWin.winObj.vline(1, int(mapWin.width/2), '=', int(mapWin.height - 2))
+
+
+		#		mainWin.winObj.border('x','x','x','x','*','*','*','*')	# ls, rs, ts, bs, tl, tr, bl, br	
+				mainWin.winObj.border()	# use default lines, and corners	
+#				mainWin.winObj.box(1, 160)
+				statsWin.winObj.border('x','x','x','x','*','*','*','*')	# ls, rs, ts, bs, tl, tr, bl, br	
+				mapWin.winObj.border('x','x','x','x','*','*','*','*')	# ls, rs, ts, bs, tl, tr, bl, br	
+#				ammoStatusWin.winObj.border('x','x','x','x','*','*','*','*')	# ls, rs, ts, bs, tl, tr, bl, br	
+
+# 				Key input needs to be in another thread, bcs waiting for prog to get out of nap
+				if stdscr.getch() == ord('a'):
+					if newPlayer.xPos > 0:
+						newPlayer.xPos -= 10
+				if stdscr.getch() == ord('d'):
+					if newPlayer.xPos < hudXPos:
+						newPlayer.xPos += 10
+				if stdscr.getch() == ord('w'):
+					if newPlayer.yPos > 0:
+						newPlayer.yPos -= 10
+					else:
+						newGame.advLvl()
+				if stdscr.getch() == ord('s'):
+					if newPlayer.yPos < curses.LINES - 1:
+						newPlayer.yPos += 10
+				if stdscr.getch() == ord('t'):
+					startAnim = not startAnim
+
+				# Collision detect, if x = x, y = y
+
+				mainWin.winObj.refresh()
+				statsWin.winObj.refresh()
+				mapWin.winObj.refresh()
+				stdscr.refresh()
+				curses.napms(100);		# draw every second
 				
+		else:	# Draw title screen
+			titleScreen.winObj.addnstr(1, 1, "Enter your name: ", 30, curses.color_pair(5))
+			titleScreen.winObj.addnstr(2, 1, "Press Ctrl-G when done.", 30, curses.color_pair(5))
+			titleScreen.winObj.border()	# use default lines, and corners	
+			titleScreen.winObj.refresh()
 
+#			User edits until press ctrl-g:
+			box.edit()
+			newPlayer.setName(box.gather())
+			newGame.startNewGame()
+	
+#			done enter name, set max level
+			titleScreen.winObj.border()	# use default lines, and corners	
+			titleScreen.winObj.refresh()
+			stdscr.refresh()
 
-		mainWin.winObj.border('x','x','x','x','*','*','*','*')	# ls, rs, ts, bs, tl, tr, bl, br	
-#		mainWin.winObj.box(1, 160)
-		statsWin.winObj.border('x','x','x','x','*','*','*','*')	# ls, rs, ts, bs, tl, tr, bl, br	
-#		oriWin.winObj.border('x','x','x','x','*','*','*','*')	# ls, rs, ts, bs, tl, tr, bl, br	
-#		ammoStatusWin.winObj.border('x','x','x','x','*','*','*','*')	# ls, rs, ts, bs, tl, tr, bl, br	
-
-#		mainWin.winObj.addch(10, 10, ord('*'))
-
-		mainWin.winObj.refresh()
-		statsWin.winObj.refresh()
-#		oriWin.winObj.refresh()
-#		ammoStatusWin.winObj.refresh()
-		stdscr.refresh()
 		curses.napms(100);		# draw every second
-
 	curses.endwin()			# deinit curses,& return terminal to previous state;echo,cbreak,keypad
 wrapper(main)			# wrapper ensures that terminal is returned to its org state, incase main fails to restore state
   		
